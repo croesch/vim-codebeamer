77dd7d78d069574b77890e1f264ff31cb17c6fdb
Christian Rösch <Christian.Roesch@icongmbh.de>
2017-10-05 16:04:32 +0200

    Add license header.

--- plugin/codebeamer_editor.py
+++ plugin/codebeamer_editor.py
@@ -1,3 +1,18 @@
+#    vim-codebeamer: Codebeamer syntax and editing support for vim.
+#    Copyright (C) 2017  Christian Roesch
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import sys
 from http.client import HTTPSConnection
 from base64 import b64encode
